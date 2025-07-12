e11b969e4ce572c9a0d452d05220dfe0a6aefbe4
Eric Koghene <erickoghene@gmail.com>
2025-07-12 12:11:21 +0000

    update

--- src/lib/api/user.js
+++ src/lib/api/user.js
@@ -18,7 +18,7 @@ export const getAuthenticatedUserProfile = async () => {
 
     try {
       
-        const response = axios.get(`/users/profile?email=${email}`);
+        const response = axios.get(`/profile?email=${email}`);
         return response.data;
     } catch (error) {
         console.error(`Erreur lors de la récupération du profil pour l'utilisateur ${email}:`, error);
